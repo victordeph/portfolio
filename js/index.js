a32f05ffd379b65f19473e62d17c98bec5cc9d82
victordeph <victorde.work@gmail.com>
2021-08-17 02:28:09 -0500

    custom

--- js/index.js
+++ js/index.js
@@ -1,5 +1,31 @@
-document.addEventListener('DOMContentLoaded', function() {
-    $(document).ready(function(){
+document.addEventListener('DOMContentLoaded', function () {
+  $(document).ready(function () {
     $('.parallax').parallax();
-    });
-    });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  $('.preloader-background').delay(1500).fadeOut('slow');
+  $('.preloader-wrapper')
+    .delay(800)
+    .fadeOut();
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  $(document).ready(function(){
+  $('.carousel').carousel({
+  dist: 0,
+  padding: 0,
+  fullWidth: true,
+  indicators: true,
+  duration: 100,
+  
+  });
+
+  autoplay()   
+  function autoplay() {
+      $('.carousel').carousel('next');
+      setTimeout(autoplay, 10000);
+  }
+  });
+});
